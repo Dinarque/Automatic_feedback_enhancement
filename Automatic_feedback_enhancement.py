d45c6922533e9771b10aecaef589c2b6fceb2873
Dinarque <96531328+Dinarque@users.noreply.github.com>
2023-09-26 17:17:57 +0200

    Update Automatic_feedback_enhancement.py

--- Automatic_feedback_enhancement.py
+++ Automatic_feedback_enhancement.py
@@ -75,7 +75,9 @@ def launch_extraction() :
         st.session_state.str_file = get_composition("buffer.pdf")
         if st.session_state.str_file == "" or st.session_state.str_file == None : 
             ocr_path("buffer.pdf")
+            st.session_state.OCR = True
             st.session_state.str_file = get_composition("buffer.pdf")
+            
             
         st.session_state.chunks= base_extractor("buffer.pdf", st.session_state.str_file)
         st.session_state.nb_chunks = len(st.session_state.chunks)
